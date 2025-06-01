eb73f98068158c2f2bb787ba3936e3d0006b3b85
CristianNieto3 <cristian.nieto20@icloud.com>
2025-06-01 17:38:30 -0500

    Added changes for CORS

--- src/main/java/com/examscheduler/backend/config/WebConfig.java
+++ src/main/java/com/examscheduler/backend/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // Required for auth headers or cookies
     }
 }
